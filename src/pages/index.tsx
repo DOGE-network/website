0f4705374cb291b4a9c54963b36e58a4cc76cb47
sarob <seanroberts66@gmail.com>
2025-09-14 11:01:25 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,13 +70,8 @@ function MissionSection() {
               Our Purpose
             </h2>
             <div className='card'>
-              <p style={{ fontSize: '1.2rem', lineHeight: '1.6', margin: 0 }}>
-                We individually are supporting various projects related to
-                government transparency. While these are generally passion
-                projects that we would do for free, it would be nice to get
-                collaboration across many similar projects to help push forward.
-                For that purpose, we have decided to start meeting in person.
-              </p>
+              <p style={{ fontSize: '1.2rem', lineHeight: '1.6', margin: 0 }}>We develop tools related to government transparency and for the public good. We implement ETL and data standards to facilitate many data sources correlation so that timely research can be turned into action. Our goal is to apply the tools and implementation to make data accessible from local, regional, and national sources.</p>
+              <p style={{ fontSize: '1.2rem', lineHeight: '1.6', margin: 0 }}>For example, the California DOGE codebase is both for California State government data research and as a template for other states to follow. Code is publicly licensed as Apache 2.0 and CC-BY. We can help you organize your team and start your own DOGE related site. We aspire to compliment [Data Republican](https://datarepublican.com) and [Federal DOGE](https://doge.gov).</p>
             </div>
           </div>
         </div>
